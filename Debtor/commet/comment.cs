8deb8e6fa02b3e0eafa6fdd3b59265e02b959e49
USER <USER@COMPUTER02>
2024-03-10 21:02:09 +0400

    Add project files.

---
+++ Debtor/commet/comment.cs
@@ -0,0 +1,31 @@
+﻿/*
+  2) rhyta.com ve ya dayrep.com domenlerinde emaili olan borclulari cixartmag
+
+	3) Yashi 26-dan 36-ya qeder olan borclulari cixartmag
+	
+	4) Borcu 5000-den cox olmayan borclularic cixartmag
+
+	5) Butov adi 18 simvoldan cox olan ve telefon nomresinde 2 ve ya 2-den cox 7 reqemi olan borclulari cixartmaq
+
+	7) Qishda anadan olan borclulari cixardmaq 
+
+	8) Borcu, umumi borclarin orta borcunnan cox olan borclulari cixarmaq ve evvel familyaya gore sonra ise meblegin azalmagina gore sortirovka etmek
+
+	9) Telefon nomresinde 8 olmayan borclularin yalniz familyasin,yashin ve umumi borcun meblegin cixarmaq
+
+	11)Adinda ve familyasinda hec olmasa 3 eyni herf olan borclularin siyahisin cixarmaq ve onlari elifba sirasina gore sortirovka elemek
+
+	13)borclulardan en coxu hansi ilde dogulubsa hemin ili cixartmaq
+
+	14)Borcu en boyuk olan 5 borclunun siyahisini cixartmaq
+
+	15)Butun borcu olanlarin borcunu cemleyib umumi borcu cixartmaq
+
+	16)2ci dunya muharibesin gormush borclularin siyahisi cixartmaq
+	
+	18)Nomresinde tekrar reqemler olmayan borclularin ve onlarin borcunun meblegin cixartmaq
+
+	19)Tesevvur edek ki,butun borclari olanlar bugunden etibaren her ay 500 azn pul odeyecekler. Oz ad gunune kimi borcun oduyub qurtara bilenlerin siyahisin cixartmaq
+
+	20)Adindaki ve familyasindaki herflerden "smile" sozunu yaza bileceyimiz borclularin siyahisini cixartmaq
+ */
